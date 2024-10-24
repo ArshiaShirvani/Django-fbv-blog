ce44fa21d614153d63d3a6deba229c0204bd60ba
Arshia <arshiashirvani.f1385@gmail.com>
2024-10-25 00:34:46 +0330

    write urls in the website app

---
+++ core/website/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+app_name='website'
+
+urlpatterns = [
+    path('',views.home,name='home'),
+    path('about',views.about,name='about'),
+    path('contact',views.contact,name='contact') ,
+    path('elements',views.elements,name='elements'),
+    path('newsletter',views.newsletter,name='newsletter'),
+    path('test',views.test,name='test'),   
+]
