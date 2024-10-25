d05ad94af100d26c499d728c95d79ca2dfa44a0a
Arshia <arshiashirvani.f1385@gmail.com>
2024-10-25 11:08:46 +0330

    Customization in admin panel

--- core/website/admin.py
+++ core/website/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from website.models import Contact,Newsletter
+
+# # Register your models here.
+class ContactAdmin(admin.ModelAdmin):
+    date_hierarchy = "created_date"
+    list_display = ('name','subject','email','message',)
+    list_filter =['name',]
+    ordering = ['created_date']
+    search_fields = ['Name','Subject','email','message']
+
+class NewsletterAdmin(admin.ModelAdmin):
+    list_display = ('email',)
+
+admin.site.register(Contact,ContactAdmin)
+admin.site.register(Newsletter,NewsletterAdmin)
+
