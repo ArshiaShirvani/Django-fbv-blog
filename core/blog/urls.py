2c863754c980436074d04c7c062a8d0d00974d8b
Arshia <arshiashirvani.f1385@gmail.com>
2024-10-25 11:16:46 +0330

    write urls about blog path

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -3,4 +3,11 @@ from . import views
 
 app_name='blog'
 
-urlpatterns = []
+urlpatterns = [
+    path('',views.homeblog,name='homeblog'),
+    path('<int:pid>',views.singleblog,name='singleblog'),
+    path('category/<str:cat_name>',views.homeblog,name='category'),
+    path('tag/<str:tag_name>',views.homeblog,name='tag'),
+    path('author/<str:author_name>',views.homeblog,name='author'),
+    path('search/',views.blog_search,name='search'),
+]
